d0992c838041407fcaf2dd4ac90a56f4c27f4a4f
USUARIO <USUARIO@DESKTOP-G7DG4D1>
2021-05-24 14:20:48 -0500

    asda

---
+++ src/main/java/com/dwa/calcu/CalculadoraController.java
@@ -0,0 +1,30 @@
+package com.dwa.calcu;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class CalculadoraController {
+	
+
+	@GetMapping("/")
+	public String index (Model modelo)
+	{
+		modelo.addAttribute("calcu",new Calculadora());
+		return "/index";
+	}
+	
+	@PostMapping("/resultado")
+	public String resultado(@ModelAttribute Calculadora calcu, Model modelo)
+	{
+		calcu.Calcular();
+		modelo.addAttribute("calcu", calcu);
+		return "/index";
+	}
+
+}
---
+++ src/test/java/com/dwa/calcu/Calculadora3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwa.calcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculadora3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
